dd508b83f7740e860d329c2ae3e8102fa886e597
S-UCHIYAMA <s.uchiyama11@gmail.com>
2025-12-11 16:40:29 +0900

    クエリビルダを使ってデータを取得しよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel基礎</title>
 </head>
 <body>
-    <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+                <td>{{ $post->created_at }}</td>
+                <td>{{ $post->updated_at }}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
